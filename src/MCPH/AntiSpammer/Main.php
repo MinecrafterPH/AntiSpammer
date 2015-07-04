57d1ee32698f3a108c92edff2f76264eb0c89ac8
Anton Magsino <MinecrafterPH@users.noreply.github.com>
2015-07-04 12:21:34 +0800

    Update Main.php

--- src/MCPH/AntiSpammer/Main.php
+++ src/MCPH/AntiSpammer/Main.php
@@ -28,7 +28,7 @@ use pocketmine\utils\TextFormat;
 		
 		public function onChat(PlayerChatEvent $event)
 		{
-		  $message = $cfg->get("kick-message")	
+		  $message = $cfg->get("kick-message");	
 		  $player = $event->getPlayer();
 		  $playerId = $player->getId(); // an identifier just for that player
 		   if(isset($this->last[$playerId]) and microtime(true) - $this->last[$playerId] < 0.5){ // if this is not the first message the player chatted, and the last message was sent less than 0.5 second ago
